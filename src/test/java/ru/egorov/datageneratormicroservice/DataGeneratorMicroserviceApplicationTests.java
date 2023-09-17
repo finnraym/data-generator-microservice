6815230e1010b4a121c6045775275d4dfc2e6c6e
Vladimir Egorov <vpegorov95@gmail.com>
2023-09-17 17:57:26 +0700

    init project

---
+++ src/test/java/ru/egorov/datageneratormicroservice/DataGeneratorMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.egorov.datageneratormicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataGeneratorMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
